6622fc753ac7590d44174f22550c086ddc27af70
Oleg Alexander <olegalexander@gmail.com>
2024-05-02 17:33:04 -0700

    Transposed ECS design to a vector of Entities.

--- app/ECSPOC.hs
+++ app/ECSPOC.hs
@@ -4,23 +4,15 @@
 
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Eta reduce" #-}
--- | Simple picture drawing application.
---   Like MSPaint, but you can only draw lines.
---
+
 module Main where
 
-import           Ant
-import           AntSim                             (pheromoneAmount)
+
 import           Data.Function                      ((&))
-import           Data.List                          (foldl', transpose, unzip5,
-                                                     zip5)
 import qualified Data.Vector                        as V
-import           Data.Vector.Generic                (foldl', new)
 import           Debug.Trace                        (traceShow)
 import           Graphics.Gloss
 import           Graphics.Gloss.Interface.Pure.Game
-import           System.Random                      (newStdGen, randoms)
-
 
 -- ----------------------------------- ECS ---------------------------------- --
 
@@ -29,67 +21,57 @@ type Angle = Float -- in radians
 type Speed = Float
 type PheremoneAmount = Float
 
-data Archetype = BlackAnt | PheremoneDrop deriving (Eq, Show)
+data EntityType = BlackAnt | PheremoneDrop deriving (Eq, Show)
 
-data World = World {
-    wTypes           :: [Archetype],
-    wPositions       :: [Maybe Position],
-    wAngles          :: [Maybe Angle],
-    wSpeeds          :: [Maybe Speed],
-    wPheremoneAmount :: [Maybe PheremoneAmount]
+data Entity = Entity {
+    eEntityType      :: EntityType,
+    ePosition        :: Maybe Position,
+    eAngle           :: Maybe Angle,
+    eSpeed           :: Maybe Speed,
+    ePheremoneAmount :: Maybe PheremoneAmount
 } deriving (Eq, Show)
 
 
+type World = V.Vector Entity
+
 initWorld :: World
-initWorld =
-    let world = World [] [] [] [] []
-        entities = [newBlackAnt (0, 0) (pi/4) 3, newPheremoneDrop (10, 10) 5]
-    in Data.List.foldl' concatWorlds world entities
+initWorld = V.fromList [newBlackAnt (0, 0) (pi/4) 3, newPheremoneDrop (10, 10) 5]
 
-concatWorlds :: World -> World -> World
-concatWorlds targetWorld sourceWorld = World {
-    wTypes           = wTypes targetWorld ++ wTypes sourceWorld,
-    wPositions       = wPositions targetWorld ++ wPositions sourceWorld,
-    wAngles          = wAngles targetWorld ++ wAngles sourceWorld,
-    wSpeeds          = wSpeeds targetWorld ++ wSpeeds sourceWorld,
-    wPheremoneAmount = wPheremoneAmount targetWorld ++ wPheremoneAmount sourceWorld
 
+newBlackAnt :: Position -> Angle -> Speed -> Entity
+newBlackAnt pos angle speed = Entity {
+    eEntityType      = BlackAnt,
+    ePosition        = Just pos,
+    eAngle           = Just angle,
+    eSpeed           = Just speed,
+    ePheremoneAmount = Nothing
 }
 
-newBlackAnt :: Position -> Angle -> Speed -> World
-newBlackAnt pos angle speed = World {
-    wTypes     = [BlackAnt],
-    wPositions = [Just pos],
-    wAngles    = [Just angle],
-    wSpeeds    = [Just speed],
-    wPheremoneAmount = [Nothing]
-}
-
-newPheremoneDrop :: Position -> PheremoneAmount -> World
-newPheremoneDrop pos amount = World {
-    wTypes     = [PheremoneDrop],
-    wPositions = [Just pos],
-    wAngles    = [Nothing],
-    wSpeeds    = [Nothing],
-    wPheremoneAmount = [Just amount]
+newPheremoneDrop :: Position -> PheremoneAmount -> Entity
+newPheremoneDrop pos amount = Entity {
+    eEntityType      = PheremoneDrop,
+    ePosition        = Just pos,
+    eAngle           = Nothing,
+    eSpeed           = Nothing,
+    ePheremoneAmount = Just amount
 }
 
 calcMovement :: Position -> Angle -> Speed -> Position
 calcMovement (x, y) angle speed = (x + speed * cos angle, y + speed * sin angle)
 
-movementSystem :: World -> World
-movementSystem world =
-    -- Notice how we pull out the fields we need
-    let positions = wPositions world
-        angles = wAngles world
-        speeds = wSpeeds world
-        positions' = zipWith3 (\pos angle speed -> case (pos, angle, speed) of
-            (Just p, Just a, Just s) -> Just $ calcMovement p a s
-            _                        -> pos) positions angles speeds
-    in world { wPositions = positions' }
+movementSystem :: Entity -> Entity
+movementSystem e =
+    -- Notice how we pull out only the fields we need
+    let position = ePosition e
+        angle = eAngle e
+        speed = eSpeed e
+    in case (position, angle, speed) of
+        (Just p, Just a, Just s) -> e { ePosition = Just (calcMovement p a s) }
+        _                        -> e
 
+-- General pattern: V.map (systemC . systemB . systemA) world
 updateWorld :: World -> World
-updateWorld world = movementSystem world
+updateWorld world = V.map movementSystem world
 
 -- -------------------------------------------------------------------------- --
 
@@ -130,15 +112,19 @@ antAccelerationRange = 0.1
 main :: IO ()
 main = do
     play (InWindow "ECS FTW!" (screenWidth, screenHeight) (20,20))
-         (greyN 0.8) fps initWorld makePicture handleEvent stepWorld
+         (greyN 0.8) fps initWorld makePictures handleEvent stepWorld
 
 
-makePicture :: World -> Picture
-makePicture (World types positions _ _ amounts) =
-    let antPics = zipWith antToPicture types positions
-    in pictures antPics
+makePictures :: World -> Picture
+makePictures world = V.map makePicture world & V.toList & pictures
+
+makePicture :: Entity -> Picture
+makePicture e =
+    let eType = eEntityType e
+        position = ePosition e
+    in antToPicture eType position
     where
-        antToPicture :: Archetype -> Maybe Position -> Picture
+        antToPicture :: EntityType -> Maybe Position -> Picture
         antToPicture BlackAnt (Just (x, y)) =
             antShapes
             & translate x y
