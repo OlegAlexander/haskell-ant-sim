684c1816ecd9c3032a5dc53b6409e7abfb58a01c
Oleg Alexander <olegalexander@gmail.com>
2024-05-14 23:23:45 -0700

    Now it's a game loop! Unless there is user input, the default msg is 'wait'.

--- app/ElmArch.hs
+++ app/ElmArch.hs
@@ -2,35 +2,41 @@
 {-# HLINT ignore "Eta reduce" #-}
 module Main where
 
-import Data.Vector.Generic (new)
 import System.Random
 
 
 -- ---------------------------------- MODEL --------------------------------- --
 
-data Model = Model {mRng :: StdGen, mSequence :: [Int]} deriving (Show)
+data Model = Model
+    { mTick :: Int,
+      mRng :: StdGen,
+      mSequence :: [Int]
+    }
+    deriving (Show)
 
 
 initModel :: Int -> Model
-initModel seed = Model (mkStdGen seed) []
+initModel seed = Model 0 (mkStdGen seed) []
 
 
 -- --------------------------------- UPDATE --------------------------------- --
 
-data Msg = Next
+data Msg = Next | Wait
 
 
 update :: Msg -> Model -> Model
 update msg model = case msg of
     Next ->
         let (n, rng') = random (mRng model)
-        in  Model rng' (n : mSequence model)
+            tick = mTick model
+        in  Model (tick + 1) rng' (n : mSequence model)
+    Wait -> model{mTick = mTick model + 1}
 
 
 -- ---------------------------------- VIEW ---------------------------------- --
 
 view :: Model -> IO ()
-view model = print $ mSequence model
+view model = print (mTick model, mSequence model)
 
 
 -- ---------------------------------- LOOP ---------------------------------- --
@@ -38,10 +44,11 @@ view model = print $ mSequence model
 loop :: Model -> IO ()
 loop model = do
     view model
-    putStrLn "Enter 'next':"
+    putStrLn "Enter 'next' or 'wait': "
     command <- getLine
     let msg = case command of
             "next" -> Next
+            "wait" -> Wait
             _ -> error "Unknown command"
     let newModel = update msg model
     loop newModel
