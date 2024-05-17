6b823ba98e5324485940c5431783c2d0383b95c6
Oleg Alexander <olegalexander@gmail.com>
2024-05-16 17:29:07 -0700

    Progress on ElmArch.

--- app/ElmArch.hs
+++ app/ElmArch.hs
@@ -7,7 +7,7 @@ import System.Random
 
 -- ---------------------------------- MODEL --------------------------------- --
 
-data Model = Model
+data GameModel = GameModel
     { mTick :: Int,
       mRng :: StdGen,
       mSequence :: [Int]
@@ -15,8 +15,10 @@ data Model = Model
     deriving (Show)
 
 
-initModel :: Int -> Model
-initModel seed = Model 0 (mkStdGen seed) []
+initModel :: IO GameModel
+initModel = do
+    seed <- randomIO
+    return $ GameModel 0 (mkStdGen seed) []
 
 
 -- --------------------------------- UPDATE --------------------------------- --
@@ -24,37 +26,40 @@ initModel seed = Model 0 (mkStdGen seed) []
 data Msg = Next | Wait
 
 
-update :: Msg -> Model -> Model
+update :: Msg -> GameModel -> GameModel
 update msg model = case msg of
     Next ->
         let (n, rng') = random (mRng model)
             tick = mTick model
-        in  Model (tick + 1) rng' (n : mSequence model)
+        in  GameModel (tick + 1) rng' (n : mSequence model)
     Wait -> model{mTick = mTick model + 1}
 
 
 -- ---------------------------------- VIEW ---------------------------------- --
 
-view :: Model -> IO ()
+view :: GameModel -> IO ()
 view model = print (mTick model, mSequence model)
 
 
 -- ---------------------------------- LOOP ---------------------------------- --
 
-loop :: Model -> IO ()
-loop model = do
-    view model
+parseMsg :: String -> Msg
+parseMsg "next" = Next
+parseMsg "wait" = Wait
+parseMsg _ = error "Unknown command"
+
+
+loop :: (GameModel -> IO ()) -> (Msg -> GameModel -> GameModel) -> GameModel -> IO ()
+loop viewFunc updateFunc model = do
+    viewFunc model
     putStrLn "Enter 'next' or 'wait': "
     command <- getLine
-    let msg = case command of
-            "next" -> Next
-            "wait" -> Wait
-            _ -> error "Unknown command"
-    let newModel = update msg model
-    loop newModel
+    let msg = parseMsg command
+    let newModel = updateFunc msg model
+    loop viewFunc updateFunc newModel
 
 
 main :: IO ()
 main = do
-    seed <- randomIO
-    loop $ initModel seed
+    model <- initModel
+    loop view update model
