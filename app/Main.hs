587db54abd0a4e101e62502e37cb363c61685217
Oleg Alexander <olegalexander@gmail.com>
2024-05-18 16:51:00 -0700

    An even more generic game loop!

--- app/Main.hs
+++ app/Main.hs
@@ -58,6 +58,9 @@ antPng :: String
 antPng = "assets/ant.png"
 
 
+type Exit = Bool
+
+
 -- TODO Recommend this function to Raylib author
 drawTextureCentered :: Texture -> Rectangle -> Float -> Float -> Vector2 -> Color -> IO ()
 drawTextureCentered texture source@(Rectangle _ _ w h) scale angle (Vector2 x y) color = do
@@ -72,18 +75,18 @@ drawTextureCentered texture source@(Rectangle _ _ w h) scale angle (Vector2 x y)
         color
 
 
-initWorld :: IO (Texture, Ant)
+initWorld :: IO (Texture, Ant, Exit)
 initWorld = do
     seed <- randomIO
     let ant = mkAnt screenCenterW screenCenterH seed
     window <- initWindow screenWidth screenHeight title
     setTargetFPS 60
     antTexture <- loadTexture antPng window
-    return (antTexture, ant)
+    return (antTexture, ant, False)
 
 
-handleInput :: (Texture, Ant) -> IO (Texture, Ant)
-handleInput (tex, ant) = do
+handleInput :: (Texture, Ant, Exit) -> IO (Texture, Ant, Exit)
+handleInput (tex, ant, exit) = do
     go <- isKeyDown KeyUp
     stop <- isKeyDown KeyDown
     left <- isKeyDown KeyLeft
@@ -93,21 +96,22 @@ handleInput (tex, ant) = do
                 { antStopGo = if go then Go else if stop then Stop else Neutral,
                   antWheelPos = if left then TurnLeft else if right then TurnRight else Center
                 }
-    return (tex, ant')
+    exit' <- windowShouldClose
+    return (tex, ant', exit')
 
 
-updateWorld :: (Texture, Ant) -> (Texture, Ant)
-updateWorld (antTexture, ant) =
+updateWorld :: (Texture, Ant, Exit) -> (Texture, Ant, Exit)
+updateWorld (antTexture, ant, exit) =
     let ant' =
             ant
                 & driveAnt antStepSize 0.33 0.33 antMaxSpeed (pi / 15) (pi / 60)
                 & cycleAntSprite antMaxSpeed
                 & wrapAroundAntRaylib (int2Float screenWidth) (int2Float screenHeight)
-    in  (antTexture, ant')
+    in  (antTexture, ant', exit)
 
 
-drawWorld :: (Texture, Ant) -> IO ()
-drawWorld (antTexture, ant) = do
+renderWorld :: (Texture, Ant, Exit) -> IO ()
+renderWorld (antTexture, ant, exit) = do
     hideCursor
 
     f11Pressed <- isKeyPressed KeyF11
@@ -131,15 +135,19 @@ drawWorld (antTexture, ant) = do
             white
 
 
+shouldExit :: (Texture, Ant, Exit) -> Bool
+shouldExit (_, _, exit) = exit
+
+
 -- A generic game loop!
-gameLoop :: (w -> IO w) -> (w -> w) -> (w -> IO ()) -> w -> IO ()
-gameLoop handleInputFunc updateFunc renderFunc world = do
+gameLoop :: (w -> IO w) -> (w -> w) -> (w -> IO ()) -> (w -> Bool) -> w -> IO ()
+gameLoop handleInputFunc updateFunc renderFunc shouldExitFunc world = do
     world' <- handleInputFunc world
-    let world'' = updateFunc world'
-    renderFunc world''
-    shouldClose <- windowShouldClose
-    unless shouldClose $ gameLoop handleInputFunc updateFunc renderFunc world''
+    unless (shouldExitFunc world') $ do
+        let world'' = updateFunc world'
+        renderFunc world''
+        gameLoop handleInputFunc updateFunc renderFunc shouldExitFunc world''
 
 
 main :: IO ()
-main = initWorld >>= gameLoop handleInput updateWorld drawWorld
+main = initWorld >>= gameLoop handleInput updateWorld renderWorld shouldExit
